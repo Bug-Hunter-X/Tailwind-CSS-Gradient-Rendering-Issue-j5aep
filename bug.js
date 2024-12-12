1740efa516a23dafa18344c5764c65381815dfba
BugHunter-X <askzenai@gmail.com>
2024-12-13 00:26:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,6 @@
+```javascript
+// Example showing the unexpected behavior
+<div class="bg-gradient-to-r from-blue-500 to-purple-500 p-4">
+  <p>This should have a gradient background, but may not.</p>
+</div>
+```
